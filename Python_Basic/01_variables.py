3575c8f8f721e41c3f573e3aa6b64d45174f745f
Angel Antonio Gomera <antoniogomera12@gmail.com>
2025-10-14 22:43:56 -0400

    mi primer commit

---
+++ Python_Basic/01_variables.py
@@ -0,0 +1,44 @@
+# Vamos a trabajar con variables
+print("\n")
+
+# Esta es la variable string
+my_string_variable: str = " Alicia quiere aprender programacion para trabajar "
+print(my_string_variable)
+
+print("\n") # Esto es para realizar un salto de linea
+
+# Esta es una variable int
+my_int_variable: int = 48
+print(my_int_variable)
+
+print("\n")
+
+# Este es un tipo de variable float
+my_float_variable: float = 10.6
+print(my_float_variable)
+
+print("\n")
+
+# Tipo de datos booleanos
+my_bool_variable: bool = True
+print(my_bool_variable)
+
+print("\n")
+
+# Tipo de datos complex
+my_complex_variable = 21 + 5j
+print(my_complex_variable)
+
+print("\n")
+
+print(type(my_string_variable))
+print(type(my_int_variable))
+print(type(my_float_variable))
+print(type(my_bool_variable))
+print(type(my_complex_variable))
+
+int # Esto es para valores enteros como 12
+float # Esto es para numeros decimales como 3.6
+str # Esto es para texto
+bool # Aqui solo tenemos 2 resultados como true o false
+complex # Este tiene una parte real y una parte imaginaria
---
+++ Python_Basic/02_operadores.py
@@ -0,0 +1,20 @@
+
+# Operadores Aritmeticos
+
+suma = 1000 + 2500
+resta = 1500 - 500
+multiplicacion = 10 * 5
+division = 10 / 3
+division_entera = 10 // 3
+modulo = 10 % 3
+exponente = 2 ** 3
+
+print(f" El resultado de la suma es: {suma}")
+print(f" El resultado de la resta es: {resta}")
+print(f" El resultado de la Multiplicacion es: {multiplicacion}")
+print(division)
+print(type(division))
+print(division_entera)
+print(type(division_entera))
+
+
